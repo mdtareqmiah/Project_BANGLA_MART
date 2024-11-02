93b1c70deb3565d4b1e2ca5206b0b51d41e82492
Md Tareq Miah <mdtareqmia25@gmail.com>
2024-11-02 07:33:23 +0600

    First Commit Front end

---
+++ BANGLA_MART/User/User.Master.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BANGLA_MART.User
+{
+    public partial class User : System.Web.UI.MasterPage
+    {
+        //private object pnlSliderUC;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Url.AbsoluteUri.ToString().Contains("Default.aspx"))
+            {
+                //load the control
+                Control sliderUserControl = (Control)Page.LoadControl("SliderUserControl.ascx");
+                pnlSliderUC.Controls.Add(sliderUserControl);
+
+            }
+           
+        }
+    }
+}
